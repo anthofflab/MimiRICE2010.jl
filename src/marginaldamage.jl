855fcc70e9a1ec000b148d08d357f72340cee847
David Anthoff <anthoff@berkeley.edu>
2015-07-14 17:09:21 -0700

    Add datafile parameter to marginalmodel stuff

--- src/marginaldamage.jl
+++ src/marginaldamage.jl
@@ -1,9 +1,9 @@
 include("rice2010.jl")
 
-function getmarginal_rice_models(;emissionyear=2005)
-    m1 = getrice()
+function getmarginal_rice_models(;emissionyear=2005,datafile="../data/RICE_2010_base_000.xlsm")
+    m1 = getrice(datafile=datafile)
 
-    m2 = getrice()
+    m2 = getrice(datafile=datafile)
     addcomponent(m2, adder, :marginalemission, before=:co2cycle)
     addem = zeros(60)
     addem[getindexfromyear_rice_2010(emissionyear)] = 1.0
