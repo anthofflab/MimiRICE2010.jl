be6881f1206a441a1633dfa0bae67ac6f6c51fdc
David Anthoff <anthoff@berkeley.edu>
2015-11-20 16:28:21 -0800

    Use enumerate in loops

--- src/helpers.jl
+++ src/helpers.jl
@@ -11,22 +11,18 @@ end
 function getparams(f, range::AbstractString, parameters, regions, T)
     if parameters == :single
         vals= Array(Float64,length(regions))
-        i=1
-        for r = regions
+        for (i,r) = enumerate(regions)
             data=readxl(f,"$r\!$range")
             vals[i]=data[1]
-            i=i+=1
         end
         return vals
     elseif parameters == :all
         vals= Array(Float64, T, length(regions))
-        i=1
-        for r = regions
+        for (i,r) = enumerate(regions)
             data=readxl(f,"$r\!$range")
             for n=1:T
                 vals[n,i] = data[n]
             end
-            i=i+=1
         end
         return vals
     end
