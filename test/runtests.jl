9bc87a28ee0281f86c897cc18abaeab6ad91da66
David Anthoff <anthoff@berkeley.edu>
2015-11-20 16:30:46 -0800

    Convert to vector instead of transpose

--- test/runtests.jl
+++ test/runtests.jl
@@ -28,44 +28,44 @@ end
 Precision = 1.0e-11
 
 #TATM Test (temperature increase)
-True_TATM = transpose(readxl(f,"Global!B70:BI70"));
+True_TATM = vec(readxl(f,"Global!B70:BI70"))
 @test_approx_eq_eps maxabs(m[:climatedynamics, :TATM] .- True_TATM) 0. Precision
 
 #MAT Test (carbon concentration atmosphere)
-True_MAT = transpose(readxl(f,"Global!B59:BI59"));
+True_MAT = vec(readxl(f,"Global!B59:BI59"))
 @test_approx_eq_eps maxabs(m[:co2cycle, :MAT] .- True_MAT) 0. Precision
 
 #DAMFRAC Test (damages fraction)
-True_DAMFRAC = Truth("B63:BI63");
+True_DAMFRAC = Truth("B63:BI63")
 @test_approx_eq_eps maxabs(m[:damages, :DAMFRAC] .- True_DAMFRAC) 0. Precision
 
 #DAMAGES Test (damages $)
-True_DAMAGES = Truth("B64:BI64");
+True_DAMAGES = Truth("B64:BI64")
 @test_approx_eq_eps maxabs(m[:damages, :DAMAGES] .- True_DAMAGES) 0. Precision
 
 #E Test (emissions)
-True_E = transpose(readxl(f,"Global!B55:BI55"));
+True_E = vec(readxl(f,"Global!B55:BI55"))
 @test_approx_eq_eps maxabs(m[:emissions, :E] .- True_E) 0. Precision
 
 #YGROSS Test (gross output)
-True_YGROSS = Truth("B61:BI61");
+True_YGROSS = Truth("B61:BI61")
 @test_approx_eq_eps maxabs(m[:grosseconomy, :YGROSS] .- True_YGROSS) 0. Precision
 
 #CPC Test (per capita consumption)
-True_CPC = Truth("B88:BI88");
+True_CPC = Truth("B88:BI88")
 @test_approx_eq_eps maxabs(m[:neteconomy, :CPC] .- True_CPC) 0. Precision
 
 #FORC Test (radiative forcing)
-True_FORC = transpose(readxl(f,"Global!B71:BI71"));
+True_FORC = vec(readxl(f,"Global!B71:BI71"))
 @test_approx_eq_eps maxabs(m[:radiativeforcing, :FORC] .- True_FORC) 0. Precision
 
 #TOTALSLR Test (total sea level rise)
-True_TOTALSLR = transpose(readxl(f,"SLR!B62:BI62"));
+True_TOTALSLR = vec(readxl(f,"SLR!B62:BI62"))
 @test_approx_eq_eps maxabs(m[:sealevelrise, :TOTALSLR] .- True_TOTALSLR) 0. Precision
 
 #SLRDAMAGES Test (damages from sea level rise)
-True_SLRDAMAGES = Truth("B50:BI50");
+True_SLRDAMAGES = Truth("B50:BI50")
 @test_approx_eq_eps maxabs(m[:sealevelrise, :SLRDAMAGES] .- True_SLRDAMAGES) 0. Precision
 
-True_UTILITY = readxl(f,"Global!B77:B77");
+True_UTILITY = readxl(f,"Global!B77:B77")
 @test_approx_eq_eps maxabs(m[:welfare, :UTILITY] .- True_UTILITY) 0. Precision
