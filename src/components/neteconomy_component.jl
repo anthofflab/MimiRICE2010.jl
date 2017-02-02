b4ce90b8ad99d488e2f22eec2692c412c4d841b9
Frank Errickson <frankerrickson@berkeley.edu>
2017-02-02 12:54:05 -0800

    Move calculations for abatement into emissions component. This affects the `ABATECOST`, `MCABATE`, and `CPRICE` variables. This fixes `Issue #7`.

--- src/components/neteconomy_component.jl
+++ src/components/neteconomy_component.jl
@@ -4,24 +4,18 @@ using Mimi
     regions = Index()
 
     YNET = Variable(index=[time, regions]) # Output net of damages equation (trillions 2005 USD per year)
-    ABATECOST = Variable(index=[time, regions]) # Cost of emissions reductions  (trillions 2005 USD per year)
-    MCABATE = Variable(index=[time, regions]) # Marginal cost of abatement (2005$ per ton CO2)
     Y = Variable(index=[time, regions]) # Gross world product net of abatement and damages (trillions 2005 USD per year)
     I = Variable(index=[time, regions]) # Investment (trillions 2005 USD per year)
     C = Variable(index=[time, regions]) # Consumption (trillions 2005 US dollars per year)
     CPC = Variable(index=[time, regions]) # Per capita consumption (thousands 2005 USD per year)
-    CPRICE = Variable(index=[time, regions]) # Carbon price (2005$ per ton of CO2)
 
     YGROSS = Parameter(index=[time, regions]) # Gross world product GROSS of abatement and damages (trillions 2005 USD per year)
     DAMFRAC = Parameter(index=[time, regions]) # Damages as fraction of gross output
     DAMAGES = Parameter(index=[time, regions]) # Damages (trillions 2005 USD per year)
-    cost1 = Parameter(index=[time, regions]) # Adjusted cost for backstop
-    MIU = Parameter(index=[time, regions]) # Emission control rate GHGs
-    expcost2 = Parameter(index=[regions]) # Exponent of control cost function
-    partfract = Parameter(index=[time, regions]) # Fraction of emissions in control regime
-    pbacktime = Parameter(index=[time, regions]) # Backstop price
+    ABATECOST = Parameter(index=[time, regions]) # Cost of emissions reductions  (trillions 2005 USD per year)
     S = Parameter(index=[time, regions]) # Gross savings rate as fraction of gross world product
     l = Parameter(index=[time, regions]) # Level of population and labor
+
 end
 
 function run_timestep(state::neteconomy, t::Int)
@@ -36,19 +30,9 @@ function run_timestep(state::neteconomy, t::Int)
         end
     end
 
-    #Define function for ABATECOST
-    for r in d.regions
-        v.ABATECOST[t,r] = p.YGROSS[t,r] * p.cost1[t,r] * (p.MIU[t,r]^p.expcost2[r]) * (p.partfract[t,r]^(1 - p.expcost2[r]))
-    end
-
-    #Define function for MCABATE
-    for r in d.regions
-        v.MCABATE[t,r] = p.pbacktime[t,r] * p.MIU[t,r]^(p.expcost2[r] - 1)
-    end
-
     #Define function for Y
     for r in d.regions
-        v.Y[t,r] = v.YNET[t,r] - v.ABATECOST[t,r]
+        v.Y[t,r] = v.YNET[t,r] - p.ABATECOST[t,r]
     end
 
     #Define function for I
@@ -70,8 +54,4 @@ function run_timestep(state::neteconomy, t::Int)
         v.CPC[t,r] = 1000 * v.C[t,r] / p.l[t,r]
     end
 
-    #Define function for CPRICE
-    for r in d.regions
-        v.CPRICE[t,r] = p.pbacktime[t,r] * 1000 * p.MIU[t,r]^(p.expcost2[r] - 1)
-    end
 end
