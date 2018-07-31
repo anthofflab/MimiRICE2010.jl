4b226b6a87a182314c08d70db0ad58411491a91a
lrennels <lrennels@berkeley.edu>
2018-07-31 14:36:35 -0700

    Update function names

--- src/marginaldamage.jl
+++ src/marginaldamage.jl
@@ -7,15 +7,15 @@ function getmarginal_rice_models(;emissionyear=2005,datafile=joinpath(dirname(@_
     m1 = mm.base
     m2 = mm.marginal
 
-    addcomponent(m2, adder, :marginalemission, before=:co2cycle)
+    add_comp!(m2, adder, :marginalemission, before=:co2cycle)
 
     time = dimension(m1, :time)
     addem = zeros(length(time))
     addem[time[emissionyear]] = 1.0
 
-    set_parameter!(m2,:marginalemission,:add,addem)
-    connect_parameter(m2,:marginalemission,:input,:emissions,:E)
-    connect_parameter(m2, :co2cycle,:E,:marginalemission,:output)
+    set_param!(m2,:marginalemission,:add,addem)
+    connect_param!(m2,:marginalemission,:input,:emissions,:E)
+    connect_param!(m2, :co2cycle,:E,:marginalemission,:output)
 
     run(m1)
     run(m2)
@@ -47,7 +47,7 @@ end
     m1 = getrice(datafile=datafile)
 
     m2 = getrice(datafile=datafile)
-    addcomponent(m2, adder, :marginalemission, before=:co2cycle)
+    add_comp!(m2, adder, :marginalemission, before=:co2cycle)
     addem = zeros(60)
     addem[getindexfromyear_rice_2010(emissionyear)] = 1.0
     setparameter(m2,:marginalemission,:add,addem)
