836cee72d7cd0db6f01abf12fbc000cad8c38473
lrennels <lrennels@berkeley.edu>
2018-11-30 15:04:31 -0800

    Use CSVFiles instead of CSV

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Test
 using ExcelReaders
 using Mimi
 using DataFrames
-using CSV
+using CSVFiles
 
 include("../src/rice2010.jl")
 using .Rice2010
@@ -101,11 +101,12 @@ for c in map(name, Mimi.compdefs(m)), v in Mimi.variable_names(m, c)
     filepath = joinpath(@__DIR__, "..", "data", "validation_data_v040", "$c-$v.csv")
     results = m[c, v]
 
+    df = load(filepath) |> DataFrame
     if typeof(results) <: Number
-        validation_results = CSV.read(filepath)[1,1]
+        validation_results = df[1,1]
         
     else
-        validation_results = convert(Array, CSV.read(filepath))
+        validation_results = convert(Array, df)
 
         #remove NaNs
         results[ismissing.(results)] .= nullvalue
